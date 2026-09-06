9b4ab79cf76fb09e4be9c4ff047f63371e08ed6e
Emil Danielsen <emil.danielsen@gmail.com>
2026-09-06 14:15:47 +0200

    Limit Earth zoom to map-equivalent level 5.35

--- src/map-view-scale.d.ts
+++ src/map-view-scale.d.ts
@@ -1,2 +1,4 @@
 export interface OrbitMapScale { zoom: number; metersPerPixel: number; altitudeKm: number }
+export const MAX_EARTH_MAP_ZOOM: number;
+export function orbitDistanceForMapZoom(input: { zoom: number; verticalFovDegrees: number; viewportHeightCssPixels: number }): number | undefined;
 export function orbitMapScale(input: { distanceEarthRadii: number; verticalFovDegrees: number; viewportHeightCssPixels: number; earthRadiusMeters?: number }): OrbitMapScale | undefined;
